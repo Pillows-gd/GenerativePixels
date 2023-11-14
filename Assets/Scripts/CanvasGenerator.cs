d4955ca2b5487728253e3dee022cf546f688fbb6
Stefano Giabelli <filippo.giabelli@gmail.com>
2023-11-14 14:59:33 +0100

    Fixed Canvas

--- Assets/Scripts/CanvasGenerator.cs
+++ Assets/Scripts/CanvasGenerator.cs
@@ -8,14 +8,12 @@ using Unity.VisualScripting;
 
 public class CanvasGenerator : MonoBehaviour
 {
-    [Range(0,1)]
-    [SerializeField] private float scale = 1;
-    [SerializeField] private int width = 100;
-    [SerializeField] private int height = 100;
+    [Range(0, 1)]
+    [SerializeField] private float scale = 0.08f;
+    [SerializeField] private int width = 128;
+    [SerializeField] private int height = 72;
     private int[,] canvas;
-    private int pixelsPerUnit = 100;
-    private Vector3 bottomLeft;
-    private Vector3 topRight;
+    private int pixelsPerUnit = 108; // Not 100! This makes the pointer more precise. I dunno why D:
 
     private void Start()
     {
@@ -26,29 +24,58 @@ public class CanvasGenerator : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 pointedPos = Input.mousePosition;
+            int[] pointedCoords = getPointedCoords();
 
-            // To Center
-            pointedPos.x -= Screen.width / 2;
-            pointedPos.y -= Screen.height / 2;
-            
-            // To Units
-            pointedPos /= pixelsPerUnit;
-
-            // To [0-1]
-            pointedPos.x += width * scale / 2;
-            pointedPos.y += height * scale / 2;
-            pointedPos.x /= width * scale;
-            pointedPos.y /= height * scale;
-            
-            // To Index
-            pointedPos.x *= width;
-            pointedPos.y *= height;
-
-            canvas[(int)pointedPos.x, (int)pointedPos.y] = 1;
-
-            print("Bottom Left = " + bottomLeft + ", Top Right = " + topRight + ". Pointing: " + pointedPos);
+            try
+            {
+                canvas[pointedCoords[0], pointedCoords[1]] = 1;
+            }
+            catch (Exception)
+            {
+                print("Pointing out of the canvas!");
+            }
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            int[] pointedCoords = getPointedCoords();
+
+            try
+            {
+                canvas[pointedCoords[0], pointedCoords[1]] = 0;
+            }
+            catch (Exception)
+            {
+                print("Pointing out of the canvas!");
+            }
+        }
+    }
+
+    int[] getPointedCoords()
+    {
+        double pointedX = Input.mousePosition.x;
+        double pointedY = Input.mousePosition.y;
+
+        // Origin To Center
+        pointedX -= Screen.width / 2;
+        pointedY -= Screen.height / 2;
+
+        // Pixels To Units
+        pointedX /= pixelsPerUnit;
+        pointedY /= pixelsPerUnit;
+
+        // Origin To Corner
+        pointedX += (double)width * scale / 2;
+        pointedY += (double)height * scale / 2;
+
+        // Units To Matrix Indices
+        pointedX /= scale;
+        pointedY /= scale;
+
+        int[] coords = new int[2];
+        coords[0] = (int)pointedX;
+        coords[1] = (int)pointedY;
+
+        return coords;
     }
 
     private void InitializeCanvas()
@@ -72,14 +99,9 @@ public class CanvasGenerator : MonoBehaviour
             {
                 for (int y = 0; y < height; y++)
                 {
-                    Vector3 pos = new Vector3(-width*.5f + .5f + x, -height*.5f + .5f + y, 0.0f);
+                    Vector3 pos = new Vector3(-width * .5f + .5f + x, -height * .5f + .5f + y, 0.0f);
 
-                    if (x == 0 && y == 0)
-                        bottomLeft = new Vector3(pos.x - .5f, pos.y - .5f, 0f) * scale;
-                    if (x == width - 1 && y == height - 1)
-                        topRight = new Vector3(pos.x + .5f, pos.y + .5f, 0f) * scale;
-
-                    Gizmos.color = canvas[x,y] == 0 ? Color.black : Color.yellow;
+                    Gizmos.color = canvas[x, y] == 0 ? Color.black : Color.yellow;
                     Gizmos.DrawCube(pos * scale, Vector3.one * scale);
                 }
             }
